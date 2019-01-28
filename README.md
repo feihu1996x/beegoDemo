20411578b6a625a5ed5de70be6b24f040b45f814
feihu1996x <gsx0312@qq.com>
2019-01-28 12:54:55 +0800

    first beego app

---
+++ README.md
@@ -0,0 +1,8 @@
+# beegoDemo
+
+> beego demo project
+
+```bash
+# install packages
+go get -v github.com/astaxie/beego
+```
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/astaxie/beego"
+
+//type RESTfulController struct {
+//	beego.Controller
+//}
+
+func main() {
+	//beego.Router("/RESTful", &RESTfulController{})
+	beego.Run()
+}
